2da0958b3e7931494a29c1d90d61af328d619f89
DeathbatO13 <danielalejo99@gmail.com>
2025-11-25 16:19:17 -0500

    Lista

--- RH-Spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ RH-Spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -1,12 +1,16 @@
 package gm.rh.controlador;
 
+import gm.rh.modelo.Empleado;
 import gm.rh.service.IEmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rh-app")
@@ -18,5 +22,13 @@ public class EmpleadoControlador {
 
     @Autowired
     private IEmpleadoServicio empleadoServicio;
+
+    @GetMapping("/empleados")
+
+    public List<Empleado> obtenerEmpleados(){
+        var empleados= empleadoServicio.listarEmpleados();
+        empleados.forEach(empleado -> logger.info(empleado.toString()));
+        return empleados;
+    }
 
 }
