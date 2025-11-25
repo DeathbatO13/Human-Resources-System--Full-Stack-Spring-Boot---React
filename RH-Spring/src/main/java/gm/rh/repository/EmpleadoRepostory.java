89b7c445979f849183c9bf3b4a27ecb1e5425975
DeathbatO13 <danielalejo99@gmail.com>
2025-11-25 16:09:26 -0500

    Servicio

---
+++ RH-Spring/src/main/java/gm/rh/repository/EmpleadoRepostory.java
@@ -0,0 +1,8 @@
+package gm.rh.repository;
+
+import gm.rh.modelo.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepostory extends JpaRepository<Empleado, Integer>{
+
+}
---
+++ RH-Spring/src/main/java/gm/rh/service/EmpleadoServicio.java
@@ -0,0 +1,35 @@
+package gm.rh.service;
+
+import gm.rh.modelo.Empleado;
+import gm.rh.repository.EmpleadoRepostory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmpleadoServicio implements IEmpleadoServicio{
+
+    @Autowired
+    private EmpleadoRepostory empleadoRepostory;
+
+    @Override
+    public List<Empleado> listarEmpleados() {
+        return empleadoRepostory.findAll();
+    }
+
+    @Override
+    public Empleado buscarEmpleadoPorId(Integer idEmpleado) {
+        return empleadoRepostory.findById(idEmpleado).orElse(null);
+    }
+
+    @Override
+    public Empleado guardarEmpleado(Empleado empleado) {
+        return empleadoRepostory.save(empleado);
+    }
+
+    @Override
+    public void eliminarEmpleado(Empleado empleado) {
+        empleadoRepostory.delete(empleado);
+    }
+}
