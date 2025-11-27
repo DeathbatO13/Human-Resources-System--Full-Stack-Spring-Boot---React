a643c31cbc2eddb7f5fbd3c514d43c084ea489f9
DeathbatO13 <danielalejo99@gmail.com>
2025-11-27 16:33:17 -0500

    Nav Bar

--- rh-react/src/App.js
+++ rh-react/src/App.js
@@ -1,9 +1,11 @@
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
 
 
 function App() {
   return (
-    <div>
+    <div className="container">
+      <Navegacion />
       <ListadoEmpleados />
     </div>
   );
